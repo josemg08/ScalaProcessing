88628e6a5543dd519fb732509d525716bff77243
jose.gonzalez <josemg08@gmail.com>
2015-03-19 11:55:54 -0300

    Initial commit

---
+++ src/Sample.scala
@@ -0,0 +1,20 @@
+import processing.core._
+
+/**
+ * Created by jose-gonzalez on 19/03/15.
+ */
+
+class Test extends PApplet {
+
+  override def setup() = {
+    size(200, 200);
+    background(0)
+    stroke(200)
+    fill(180)
+    textSize(10)
+  }
+
+  override def draw {
+  }
+
+}
