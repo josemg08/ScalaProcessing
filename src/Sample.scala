0a471ff3b9dfd085775bf59fe7d8b02dcd421dd6
jose.gonzalez <josemg08@gmail.com>
2015-03-19 14:13:40 -0300

    For sample whit circles.

--- src/Sample.scala
+++ src/Sample.scala
@@ -1,20 +1,29 @@
 import processing.core._
 
 /**
- * Created by jose-gonzalez on 19/03/15.
+ * Created by Jose Gonzalez on 19/03/15.
  */
 
 class Test extends PApplet {
 
+  var tamano = 0;
+
   override def setup() = {
-    size(200, 200);
-    background(0)
-    stroke(200)
-    fill(180)
-    textSize(10)
+    size(300,300)
+    noFill()
+    stroke(255,0,0)
   }
 
-  override def draw {
+  override def draw: Unit = {
+    background(0);
+    var i = 0;
+    for(i <- 5 to 25 by 10) {
+      ellipse(150, 150, tamano + i, tamano + i)
+    }
+    tamano = tamano + 1
+    if(tamano > 80){
+      tamano = 1
+    }
   }
 
 }
