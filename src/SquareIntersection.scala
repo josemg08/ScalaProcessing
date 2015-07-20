f780bc53166e51b2e4881f56db3ad7a3cf768dcf
josemg08 <josemg08@gmail.com>
2015-07-20 14:21:59 -0300

    Drawing circles in angle.

--- src/SquareIntersection.scala
+++ src/SquareIntersection.scala
@@ -15,11 +15,46 @@ class SquareIntersection extends PApplet {
     background(0)
   }
 
-  override def draw(): Unit = {
+  override def draw() {
   }
 
-  override def mousePressed(): Unit = {
+  override def mousePressed() = {
     point(mouseX, mouseY)
+    //drawCircle(mouseX, mouseY)
+
+    val length = 20
+    val angleJump = Math.PI / 6
+    var angle = 0.0
+    var circleX = 0.0
+    var circleY = 0.0
+
+    //.___ Go through all angles from 0 to 2 * PI radians __./
+    while (angle < 2 * Math.PI) {
+      //.___ calculate x, y from a vector with known length and angle __./
+      circleX = length * Math.cos(angle)
+      circleY = length * Math.sin(angle)
+
+      point((mouseX+circleX).toInt, (mouseY+circleY).toInt)
+      angle += angleJump
+    }
   }
+
+  /*def drawCircle(x : Int, y : Int) = {
+    val length = 20
+    val angleJump = 0.3
+    var angle = 0
+    var circleX = 0.0
+    var circleY = 0.0
+
+    //.___ Go through all angles from 0 to 2 * PI radians __./
+    while (angle < 2 * Math.PI) {
+      //.___ calculate x, y from a vector with known length and angle __./
+      circleX = length * Math.cos(angle)
+      circleY = length * Math.sin(angle)
+
+      point(x+circleX, y+circleY)
+      angle += angleJump
+    }
+  }*/
 
 }
